9fa263e797ef23b71ed125b989a57acf357cb8b0
AT0MBE5T <atombest999@gmail.com>
2026-05-27 15:57:44 +0300

    test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -75,23 +75,24 @@ export const handle: Handle = async ({ event, resolve }) => {
 };
 
 async function tryServerRefresh(svelteFetch: typeof fetch, cookies: Cookies) {
-    try{
-        const refreshToken = cookies.get('refreshToken');
-        const response = await svelteFetch(`${env.PUBLIC_API_URL}/api/refreshes/refresh`, {
-            method: "POST",
-            headers: {
-                cookie: `refreshToken=${refreshToken}`
-            }
-        });
-        if (!response.ok){
-            return null;
-        }
-        const data = await response.json();
-        settings.online = true;
-        return data.token as string;
-    }catch{
-        settings.online = false;
-    }
+    return 'test';
+    // try{
+    //     const refreshToken = cookies.get('refreshToken');
+    //     const response = await svelteFetch(`${env.PUBLIC_API_URL}/api/refreshes/refresh`, {
+    //         method: "POST",
+    //         headers: {
+    //             cookie: `refreshToken=${refreshToken}`
+    //         }
+    //     });
+    //     if (!response.ok){
+    //         return null;
+    //     }
+    //     const data = await response.json();
+    //     settings.online = true;
+    //     return data.token as string;
+    // }catch{
+    //     settings.online = false;
+    // }
 }
 
 const getUserDto = async (token: string): Promise<UserDto | null> => {
